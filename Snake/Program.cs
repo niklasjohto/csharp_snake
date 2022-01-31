b6954a5f39d852129245e3f0c4f63eaed7d752f8
niklasjohto <niklasjohto@tutanota.com>
2022-01-31 22:33:40 +0100

    Fixed tail delay

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -4,15 +4,15 @@ using System.Drawing;
 namespace Snake {
     class Program {
         static void Main() {
-            Snake snake = new Snake();
+            Snake snake = new Snake(10, 30);
             snake.Start();
         }
     }
     class Snake {
-        private readonly int[,] _map = new int[10, 30];
+        private readonly int[,] _map;
 
         private string _snakeChar = "\u2588";
-        private string _fruitChar = "#";
+        private string _fruitChar = "@";
         private string _tileChar = "\u2591";
 
         private Point _fruitPos = new Point();
@@ -26,6 +26,15 @@ namespace Snake {
         private int _ySpeed = 0;
 
         private bool _gameOver = false;
+
+        public Snake(int ySize, int xSize) {
+            _map = new int[ySize, xSize];
+
+            _y = _map.GetLength(0) / 2;
+            _x = _map.GetLength(1) / 2;
+
+            _tail.Add(new Point(_x, _y));
+        }
 
         void GameOver() {
             _gameOver = true;
@@ -58,13 +67,15 @@ namespace Snake {
 
             _fruitPos = new Point(randomX, randomY);
 
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.SetCursorPosition(_fruitPos.X, _fruitPos.Y);
             Console.Write(_fruitChar);
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         void Update() {
             // Replace the last tail's position with a tile character
-            Console.SetCursorPosition(_tail.First().X, _tail.First().Y);
+            Console.SetCursorPosition(_tail.Last().X, _tail.Last().Y);
             Console.Write(_tileChar);
 
             if (_x == _fruitPos.X & _y == _fruitPos.Y) {
@@ -72,25 +83,29 @@ namespace Snake {
                 _tail.Add(new Point(_tail.First().X, _tail.First().Y));
             }
 
-            for (int i = 0; i < _tail.Count - 1; i++) {
-                _tail[i] = _tail[i + 1];
+            for (int i = _tail.Count - 1; i > 0; i--) {
+                _tail[i] = _tail[i - 1];
             }
 
-            _tail[_tail.Count - 1] = new Point(_x, _y);
+            // New head
+            _tail[0] = new Point(_x, _y);
 
             _x += _xSpeed;
             _y += _ySpeed;
 
             // Check if snake is outside playable area
-            if (_x > _map.GetLength(1) | (_x + 2) == 0 | _y > _map.GetLength(0) | (_y + 2) == 0) {
+            if (_x > _map.GetLength(1) | (_x + 2) <= 0 | _y > _map.GetLength(0) | (_y + 2) <= 0) {
                 GameOver();
                 return;
             }
 
-            foreach(Point pos in _tail) {
+            foreach (Point pos in _tail) {
                 Console.SetCursorPosition(pos.X, pos.Y);
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(_snakeChar);
             }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         void KeyPress(ConsoleKeyInfo press) {
@@ -131,13 +146,8 @@ namespace Snake {
         public void Start() {
             Console.CursorVisible = false;
 
-            _y = _map.GetLength(0) / 2;
-            _x = _map.GetLength(1) / 2;
-
             DrawMap();
             Fruit();
-
-            _tail.Add(new Point(_x, _y));
 
             GameLoop();
         }
